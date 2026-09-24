497e4e10fe9c3cbdab2ceae22022e63d3ef08e11
yurigranz <yuriiskiller@gmail.com>
2026-09-24 19:19:42 -0300

    Adicionar nova funcionalidade ao projeto

---
+++ nova_funcionalidade.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+def nova_funcionalidade():
+    print("Nova funcionalidade adicionada ao sistema.")
+    print("Artefato criado durante a atividade prática.")
+
+
+nova_funcionalidade()
---
+++ sistema.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+def apresentar_sistema():
+    print("Sistema de Gestão de Configuração")
+    print("Versão 1.0")
+
+
+apresentar_sistema()
